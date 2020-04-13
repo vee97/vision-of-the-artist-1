7aac4083fc9c00982631332f54dc423be88c9b52
vee97 <33605479+vee97@users.noreply.github.com>
2020-04-13 18:33:13 -0400

    Add custom fade script

---
+++ Vision of the Artist-1/Assets/Scripts/FadeInSprite.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+
+    SpriteRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        Color c = rend.material.color;
+        c.a = 0f;
+        rend.material.color = c;
+        
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (float f = 0.05f; f <= 1; f += 0.05f)
+        {
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.05fs);
+        }
+    }
+
+    public void startFading()
+    {
+        StartCoroutine("FadeIn");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Vision of the Artist-1/Assets/Scripts/FadeOut.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour
+{
+    SpriteRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+
+    }
+
+    IEnumerator Fadeout()
+    {
+        for (float f = 1f; f >= -0.05f; f -= 0.05f)
+        {
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+
+        }
+    }
+
+    public void startFadingOut()
+    {
+        StartCoroutine("Fadeout");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
