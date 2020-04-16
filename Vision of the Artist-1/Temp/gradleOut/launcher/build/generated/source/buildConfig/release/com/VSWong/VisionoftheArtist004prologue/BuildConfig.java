5dd8b28f5dcc9ba72477f709b86dee8d97598ab5
vee97 <33605479+vee97@users.noreply.github.com>
2020-04-16 19:05:12 -0400

    Fixed

---
+++ Vision of the Artist-1/Temp/gradleOut/launcher/build/generated/source/buildConfig/release/com/VSWong/VisionoftheArtist004prologue/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.VSWong.VisionoftheArtist004prologue;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.VSWong.VisionoftheArtist004prologue";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
